0e2fe264238e78d9430f8fecb5c45f9e95a8d0a1
Gianmarco <gianmfed@gmail.com>
2022-06-21 19:17:58 +0200

    datetime

--- p19CountingSundays.py
+++ p19CountingSundays.py
@@ -10,13 +10,15 @@
 # A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+import datetime
 
 days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
 months = []
 
+
 # Conta solo il numero di giorni registrando poi l'ultima domenica
 # Es. 1900 -> 365 / 7 poiché inizia con lunedì l'ultimo giorno è domenica
 # Far iniziare gli anni dal lunedì???
 
-# Usare modulo time con mktime() e struct_time
+# Usare modulo datetime
