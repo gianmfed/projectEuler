eef8b3ad9070668e14fffc73aa7ff9c03e2f9434
Gianmarco <gianmfed@gmail.com>
2022-07-07 15:44:38 +0200

    modifica non funziona

--- p21AmicableNumbers.py
+++ p21AmicableNumbers.py
@@ -5,23 +5,37 @@
 # The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 # Evaluate the sum of all the amicable numbers under 10000.
+from math import sqrt
 
 sum_of_amicable_numbers = 0
 
+c_iterations = 0
+
 def sum_of_divisors(n):
     sum = 0
-    for d in range(1, n):
+    for d in range(1, int(sqrt(n))):#################### solo n e non sqrt(n)
+        global c_iterations
+        c_iterations += 1
         if (n % d == 0):
+            # print(f'{d} and {n/d}')
             sum += d
+            if (d != n // d):############################
+                print(d)#################################
+                sum += n // d############################
     
     return sum
 
 
 
 for number in range(1, 10000):
+    c_iterations += 1
     d_a = sum_of_divisors(number)
 
     if sum_of_divisors(d_a) == number and number != d_a:
         sum_of_amicable_numbers += number
     
+print(f'{c_iterations} iterations')
 print(sum_of_amicable_numbers)
+
+# output 31626
+# 82221805 iterations
