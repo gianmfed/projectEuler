4cb7afa36f6a5f2aa6e3ab01e28e77761b1fd450
Gianmarco <gianmfed@gmail.com>
2022-09-28 11:36:55 +0200

    p22 light edited

--- p22NamesScores.py
+++ p22NamesScores.py
@@ -1,7 +1,29 @@
 import csv
 # import pandas
-
-with open('p22names.txt') as file:
+names = list()
+with open('p022_names.txt') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
+        names = row
+
+names.sort()
+
+zeppa = ord('A') - 1
+
+summation = 0
+value = 0
+count = 0
+while True:
+    count += 1
+    # print(f'{count} {ord(names[count - 1][0]) - zeppa}')
+    try:
+        for letter in names[count - 1]:
+            value += ord(letter) - zeppa
+        summation += (count * value)
+        value = 0
+    except:
+        break
+    # if names[count - 1] == "COLIN":
+    #     break
+
+print(summation)
